23fd6ff936b23264c4b31dc0c6ccc7cb83ebddb2
JRonca <jrfr21112000@gmail.com>
2021-04-05 22:37:28 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+export default function Home() {
+  return (
+    <div>
+      <CssBaseline />
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
+}
